7443cca039302657427b6e6c4ef1dae0c3770b1d
Martijn Laarhoven <martijnlaarhoven2001@gmail.com>
2025-11-24 09:49:59 +0100

    Second commit

---
+++ clear.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#remove LongRangeDihadron content
+rm -r ProcessOutput
+rm -r TemplateFit
+rm -r FourierFit
+
+mkdir ProcessOutput
+mkdir ProcessOutput/PtDiff
+mkdir TemplateFit
+mkdir TemplateFit/PDFs
+mkdir TemplateFit/PtDiff
+mkdir TemplateFit/PtDiff/PDFs
+mkdir FourierFit
+mkdir FourierFit/PDFs
+mkdir FourierFit/PtDiff
+mkdir FourierFit/PtDiff/PDFs
